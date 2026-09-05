068fb60fdbe78938607a8614ddba044c0b6d7827
d0vere <97129466+d0vere@users.noreply.github.com>
2026-09-05 07:50:46 +0200

    Expose pairing store to SwiftUI

--- NavidromeMusicSync/NavidromeMusicSyncApp.swift
+++ NavidromeMusicSync/NavidromeMusicSyncApp.swift
@@ -3,11 +3,13 @@ import SwiftUI
 @main
 struct NavidromeMusicSyncApp: App {
     @StateObject private var model = AppModel()
+    @StateObject private var pairingStore = PairingFileStore()
 
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .environmentObject(model)
+                .environmentObject(pairingStore)
         }
     }
 }
